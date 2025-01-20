adc6f85461dc56b40903624f18567d52c8890cd4
nicholas <aigberuan6@gmail.com>
2025-01-20 13:09:20 +0100

    2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,10 +93,10 @@ const config = {
                 ]
   		},
   		backgroundImage: {
-  			appointments: 'url('/assets/images/appointments-bg.png')',
-  			pending: 'url('/assets/images/pending-bg.png')',
-  			cancelled: 'url('/assets/images/cancelled-bg.png')'
-  		},
+			appointments: "url('/assets/images/appointments-bg.png')",
+			pending: "url('/assets/images/pending-bg.png')",
+			cancelled: "url('/assets/images/cancelled-bg.png')",
+		},
   		keyframes: {
   			'accordion-down': {
   				from: {
